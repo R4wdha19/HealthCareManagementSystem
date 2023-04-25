3a58c0ed7a265e76390c7dafdd48bd71edeb49bc
R4wdha19 <116624370+R4wdha19@users.noreply.github.com>
2023-04-25 11:50:50 +0400

    initial Comment

---
+++ src/main/java/com/codeline/springbootpractice/HealthCareMAS/HealthCareMasApplication.java
@@ -0,0 +1,13 @@
+package com.codeline.springbootpractice.HealthCareMAS;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HealthCareMasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HealthCareMasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/codeline/springbootpractice/HealthCareMAS/HealthCareMasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codeline.springbootpractice.HealthCareMAS;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HealthCareMasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
